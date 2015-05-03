8ce1a4ff69a5e3161114ddfce890a6a779eb36c6
Otter <otc@otterthecat.com>
2015-05-03 17:27:43 -0500

    extracted functions to helper module

--- thing.js
+++ thing.js
@@ -1,42 +1,7 @@
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
-var Awrap = require('./Awrap');
-
-var applyProps = function(obj, key){
-	Object.defineProperty(obj, key, {
-		"get": function(){
-			return obj.props[key];
-		},
-		"set": function(val){
-			obj.props[key] = val;
-			obj.emit('change', obj.props);
-			obj.emit('change:' + key, obj.props[key]);
-		},
-		"enumerable": true,
-		'writeable': true,
-		'configurable': true
-	});
-};
-
-var applyFakeArray = function(obj, key, orig){
-	var self = obj;
-	self.props[key] = new Awrap(orig[key]);
-	Object.defineProperty(self, key, {
-		"get": function(){
-			return self.props[key];
-		},
-		"set": function(val){
-			self.props[key] = val;
-			self.emit('change:' + key, val);
-		}
-	});
-	self.props[key] = new Awrap(orig[key]);
-	self.props[key].on('change', function(data){
-		self.emit('change:'+key, data);
-	});
-};
-
+var helpers = require('./helpers');
 
 var Foo = function(opts){
 	EventEmitter.call(this);
@@ -45,9 +10,9 @@ var Foo = function(opts){
 	for(var item in opts){
 		this.props[item] = opts[item];
 		if(opts[item].unshift){
-			applyFakeArray(this, item, opts);
+			helpers.applyFakeArray(this, item, opts);
 		} else {
-			applyProps(this, item);
+			helpers.applyProps(this, item);
 		}
 	}
 };
