6d0a21fcb305149ea1683626420be48ef0b48a40
Otter <otc@otterthecat.com>
2015-05-14 19:18:36 -0500

    removed otest.js. Updated examples. Renamed littlefingsrs.js to example.js.

---
+++ example.js
@@ -0,0 +1,24 @@
+var helpers = require('./helpers');
+
+var foo = helpers.wrapLiteral({
+    'bar': 1,
+    'baz': {
+        'bing': 'boom'
+    },
+    'booz': [1]
+});
+
+foo.on('change:bar', function(val){
+    console.log('foo.bar has changed to = ', val);
+})
+.on('change:baz:bing', function(val){
+    console.log('foo.baz.bing has changed to ', val);
+})
+.on('change:booz', function(val){
+    console.log('foo.booz array has been updated with ', val);
+});
+
+console.log('current bar ', foo.bar);
+foo.bar = 2;
+foo.booz.push(66);
+foo.baz.bing = 'blue';
